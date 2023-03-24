8d2df768bb43aab3718fadf31f30a105b40d31f7
Collinsolayemi <collinsolayemi@gmail.com>
2023-03-24 13:18:39 -0700

    creating a new user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+}
