2e12cd6bd092a18a062a63ecc698e625fdb008bd
Collinsolayemi <collinsolayemi@gmail.com>
2023-03-23 17:13:55 -0700

    using typeorm in the module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User } from './users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
