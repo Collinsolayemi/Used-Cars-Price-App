4802256655945cc9a5b9042a1ac68f969aa3a0c1
Collinsolayemi <collinsolayemi@gmail.com>
2023-03-24 02:47:22 -0700

    report entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/users.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
@@ -12,7 +13,7 @@ import { User } from './users/users.entity';
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Report],
       synchronize: true,
     }),
     UsersModule,
