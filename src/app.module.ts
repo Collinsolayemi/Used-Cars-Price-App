1e415663f2e4a5d476e51477834b0e829e01ff51
Collinsolayemi <collinsolayemi@gmail.com>
2023-03-23 17:21:05 -0700

    code restructure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/users.entity';
 
 @Module({
   imports: [
+    //The typeOrm.forRoot() create the database
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
     UsersModule,
