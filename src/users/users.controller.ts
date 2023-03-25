ef04724df8fd21af6520daa8597dad276b0463fa
Collinsolayemi <collinsolayemi@gmail.com>
2023-03-25 16:38:45 -0700

    user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Patch,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
 
@@ -9,4 +17,15 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password);
   }
+
+  @Get()
+  getAllUsers() {}
+
+  @Get('/:id')
+  getUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Patch()
+  update() {}
 }
