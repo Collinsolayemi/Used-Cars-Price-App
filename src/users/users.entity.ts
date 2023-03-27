1c419a759d4ab5053d96df5665d02a12776cd4ba
Collinsolayemi <collinsolayemi@gmail.com>
2023-03-27 16:50:09 -0700

    exclisung password from response object

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -6,6 +6,7 @@ import {
   Column,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
@@ -16,6 +17,7 @@ export class User {
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @AfterInsert()
