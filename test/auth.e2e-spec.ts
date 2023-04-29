25367e0c84ab538f640c66bbc407093c98a61029
Collinsolayemi <collinsolayemi@gmail.com>
2023-04-28 20:14:53 -0700

    end to end testing

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -27,4 +27,22 @@ describe('Authentication system', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('signup a new user then get the currently signed in user', async () => {
+    const email = 'test@example.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'test' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    // const { body } = await request(app.getHttpServer())
+    //   .get('auth/whoami')
+    //   .set('Cookie', cookie)
+    //   .expect(200);
+
+    // expect(body.email).toEqual(email);
+  });
 });
