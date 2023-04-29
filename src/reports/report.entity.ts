997e3913a690d3977e43ba91fae47d281fa504de
Collinsolayemi <collinsolayemi@gmail.com>
2023-04-28 23:16:54 -0700

    report entity

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -7,4 +7,22 @@ export class Report {
 
   @Column()
   price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  longitude: number;
+
+  @Column()
+  latitude: number;
+
+  @Column()
+  mileage: number;
 }
