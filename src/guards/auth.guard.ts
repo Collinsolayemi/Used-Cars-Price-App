4e41c133d8c9a9be20fc6c27e7be59342b6df0c8
Collinsolayemi <collinsolayemi@gmail.com>
2023-05-18 17:12:33 -0700

    code resructure

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -6,7 +6,6 @@ export class AuthGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-   console.log(request.user)
     return true
   }
 }
