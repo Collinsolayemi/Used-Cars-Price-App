9032423dda114f744ee5ee37bbdd74756d0b22bc
Collinsolayemi <collinsolayemi@gmail.com>
2023-06-03 16:05:42 -0700

    debugging

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,36 +1,36 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/auth/auth.service';
 
-// export class AuthGuard implements CanActivate {
-//   constructor(private readonly jwtService: JwtService) {}
-//   canActivate(
-//     context: ExecutionContext,
-//   ): boolean | Promise<boolean> | Observable<boolean> {
-//     const request = context.switchToHttp().getRequest();
 
-//     const token = request.headers.authorization?.split(' ')[1];
-//     if (token) {
-//       try {
-//         const decodedToken = this.jwtService.verify(token);
-//         console.log(decodedToken)
-//         request.user = decodedToken;
-//         return true;
-//       } catch (error) {
-//         return false;
-//       }
-//     }
 
-//     return false;
-//   }
-// }
 
-@Injectable()
+ @Injectable()
 export class AuthGuard implements CanActivate {
+  //constructor(private readonly auth: AuthService) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    return true;
+
+    const token = request.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+
+    //authentication logic here
+    // const isValidToken = this.auth.validateToken(token);
+
+    // if (!isValidToken) {
+    //   throw new UnauthorizedException('Invalid token');
+    // }
+
+    return false;
   }
 }
